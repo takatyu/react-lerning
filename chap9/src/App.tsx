19787ed62a5dfebba5b9d976617f314dfef57293
taka.hiroto <takaih.dev57@gmail.com>
2022-05-03 17:56:50 +0900

    カスタムフック前準備

--- chap9/src/App.tsx
+++ chap9/src/App.tsx
@@ -1,25 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+export type User = {
+  id: number;
+  name: string;
+  lastname: string;
+  firstname: string;
+  age: number;
+};
+
 function App() {
+
+  const [userList, setUserList] = useState<User[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  // ボタン押下イベント
+  const onclickFetchUser = () => {
+    // ボタン押下ローディングフラグON, エラーフラグOFF
+    setIsLoading(true);
+    setIsError(false);
+
+    // API取得
+    axios.get<User[]>('http://localhost:9000/')
+      .then(result => {
+        const users = result.data.map(user => ({
+          id: user.id,
+          lastname: user.lastname,
+          firstname: user.firstname,
+          name: `${user.lastname} ${user.firstname}`,
+          age: user.age,
+        }));
+        setUserList(users);
+      })
+      .catch(() => setIsError(true))
+      .finally(() => setIsLoading(false));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={onclickFetchUser}>ユーザ取得</button>
+      {isError && <p style={{ color: "red" }}>エラーが発生しました。</p>}
+      {isLoading ? ( <p>データ取得中です。</p> ) : (
+          userList.map((user => (
+            <p key={user.id}>{user.id}:{user.name} ({user.age})</p>
+          )))
+          )}
+    </>
   );
 }
 
