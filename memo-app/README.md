dca1d8739d577c003e3ac791e07cb37489094dee
taka.hiroto <takaih.dev57@gmail.com>
2022-05-05 21:54:47 +0900

    メモアプリ_画面モック

--- memo-app/README.md
+++ memo-app/README.md
@@ -44,3 +44,10 @@ You don’t have to ever use `eject`. The curated feature set is suitable for sm
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
 To learn React, check out the [React documentation](https://reactjs.org/).
+
+## create-react-app
+`npx create-react-app --template typescript memo-app`
+
+### install styled-components
+
+`yarn add styled-components @types/styled-components`
--- memo-app/src/App.tsx
+++ memo-app/src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChangeEvent, useState, FC } from "react";
+import styled from "styled-components"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>簡単メモアプリ</h1>
+      <input type="text" />
+      <SButton>追加</SButton>
+      <SContainer>
+        <p>メモ一覧</p>
+        <ul>
+          <li>
+            <SMemoWrapper>
+              <p>本を読む</p>
+              <SButton>削除</SButton>
+            </SMemoWrapper>
+          </li>
+          <li>
+            <SMemoWrapper>
+              <p>テスト</p>
+              <SButton>削除</SButton>
+            </SMemoWrapper>
+          </li>
+        </ul>
+      </SContainer>
     </div>
   );
 }
 
 export default App;
+
+const SButton = styled.button`
+  margin-left: 16px;
+`;
+
+const SContainer = styled.div`
+  border: solid 1px #ccc;
+  padding: 16px;
+  margin: 8px;
+`;
+
+const SMemoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
